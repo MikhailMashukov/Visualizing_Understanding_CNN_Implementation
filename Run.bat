8f0861c7a62114c575a51cb284fd17d4540f2877
Mikhail Mashukov <mashukovm@mail.ru>
2019-09-25 11:28:45 +0700

    Useful additions/utilities

---
+++ Run.bat
@@ -0,0 +1,5 @@
+@rem set CUDA_VISIBLE_DEVICES=-1
+@rem set PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v9.0\bin;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v9.0\libnvvp;%PATH%
+@mkdir NetLogs
+@copy *.py NetLogs\
+"C:\Program Files\Python35\python.exe" %1 2>err
